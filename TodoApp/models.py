0fce285a86ecd48d199f2d90961fa0a9c51cd966
javiersaavedraalcala <javiersaavedradev@gmail.com>
2025-02-11 13:03:40 -0600

    mysql added

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -5,19 +5,19 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 class Users(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True)
-    username = Column(String, unique=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    hashed_password = Column(String)
+    email = Column(String(255), unique=True)
+    username = Column(String(255), unique=True)
+    first_name = Column(String(100))
+    last_name = Column(String(100))
+    hashed_password = Column(String(255))
     is_active = Column(Boolean, default=True)
-    role = Column(String)
+    role = Column(String(50))
 
 class Todos(Base):
     __tablename__ = "todos"
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    description = Column(String)
+    title = Column(String(200))
+    description = Column(String(1000))
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
     owner_id = Column(Integer, ForeignKey("users.id"))
